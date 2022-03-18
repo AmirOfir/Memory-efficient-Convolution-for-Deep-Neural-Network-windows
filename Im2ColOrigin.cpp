e275a0556e4d905da5f5c76358558844459bc8b3
Amir Ofir <amir.israel.ofir@gmail.com>
2022-03-18 11:08:26 +0200

    Update Im2ColOrigin.cpp
    Export to function, parametrize

--- Im2ColOrigin.cpp
+++ Im2ColOrigin.cpp
@@ -13,7 +13,7 @@ void gemm_nn(int M, int N, int K, float ALPHA,
     float* C, int ldc)
 {
     int i, j, k;
-#pragma omp parallel for
+//#pragma omp parallel for
     for (i = 0; i < M; ++i) {
         for (k = 0; k < K; ++k) {
             float A_PART = ALPHA * A[i * lda + k];
@@ -57,45 +57,50 @@ void im2col_cpu(float** src, const int &inHeight, const int &intWidth, const int
 }
 
 
-const int kernel_num = 64;
-const int kernel_h = 7;
-const int kernel_w = 7;
-const int inHeight = 224;
-const int inWidth = 224;
+constexpr int mec_im2col_orig_kernel_num = 64;
+constexpr int mec_im2col_orig_kernel_h = 7;
+constexpr int mec_im2col_orig_kernel_w = 7;
+constexpr int mec_im2col_orig_in_height = 224;
+constexpr int mec_im2col_orig_in_hidth = 224;
 
-int main(){
+// Currently supports ONLY single input channel
+
+float measure_im2col_time(int repeat_count, int out_channels = mec_im2col_orig_kernel_num,
+    int kernel_h = mec_im2col_orig_kernel_h, int kernel_w = mec_im2col_orig_kernel_w,
+    int in_height = mec_im2col_orig_in_height, int in_width = mec_im2col_orig_in_hidth)
+{
     // 构造输入矩阵
-    float **src = new float*[inHeight];
-    for(int i = 0; i < inHeight; i++){
-        src[i] = new float[inWidth];
-        for(int j = 0; j < inWidth; j++){
+    float** src = new float* [in_height];
+    for (int i = 0; i < in_height; i++) {
+        src[i] = new float[in_width];
+        for (int j = 0; j < in_width; j++) {
             src[i][j] = 0.1;
         }
     }
     // 构造kernel矩阵
-    float **kernel[kernel_num];
-    for(int i = 0; i < kernel_num; i++){
-        kernel[i] = new float*[kernel_h];
-        for(int j = 0; j < kernel_h; j++){
+    float*** kernel = new float**[out_channels];
+    for (int i = 0; i < out_channels; i++) {
+        kernel[i] = new float* [kernel_h];
+        for (int j = 0; j < kernel_h; j++) {
             kernel[i][j] = new float[kernel_w];
-            for(int k = 0; k < kernel_w; k++){
+            for (int k = 0; k < kernel_w; k++) {
                 kernel[i][j][k] = 0.2;
             }
         }
     }
 
-    
+
     // 对kernel进行Im2col
-    float* kernel2col = new float[kernel_num*kernel_h*kernel_w];
+    float* kernel2col = new float[out_channels * kernel_h * kernel_w];
     int cnt = 0;
-    for(int i = 0; i < kernel_num; i++){
-        for(int j = 0; j < kernel_h; j++){
-            for(int k = 0; k < kernel_w; k++){
+    for (int i = 0; i < out_channels; i++) {
+        for (int j = 0; j < kernel_h; j++) {
+            for (int k = 0; k < kernel_w; k++) {
                 kernel2col[cnt++] = kernel[i][j][k];
             }
         }
     }
-    
+
     // 开始计时
     // Changed using: https://www.cplusplus.com/reference/ctime/time/
     struct tm y2k = { 0 };
@@ -104,55 +109,69 @@ int main(){
 
     auto start = std::clock();
 
-    // 对输入矩阵Im2col
-    int outHeight = inHeight - kernel_h + 1;
-    int outWidth = inWidth - kernel_w + 1;
-    float *srcIm2col = new float[kernel_w * kernel_h * outWidth * outHeight];
-    im2col_cpu(src, inHeight, inWidth, kernel_h, kernel_w, srcIm2col);
+    for (int repeat_ix = 0; repeat_ix < repeat_count; ++repeat_ix)
+    {
+        // 对输入矩阵Im2col
+        int outHeight = in_height - kernel_h + 1;
+        int outWidth = in_width - kernel_w + 1;
+        float* srcIm2col = new float[kernel_w * kernel_h * outWidth * outHeight];
+        im2col_cpu(src, in_height, in_width, kernel_h, kernel_w, srcIm2col);
 
-    // 使用Blas库实现矩阵乘法
-    float *output = new float[kernel_num * outHeight * outWidth];
+        // 使用Blas库实现矩阵乘法
+        float* output = new float[out_channels * outHeight * outWidth];
 
 #if _WIN64
-    gemm_nn(
+        gemm_nn(
 #else
-    cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans,
+        cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans,
 #endif
-        /* M: */        kernel_num,
-        /* N: */        outHeight * outWidth, 
-        /* K: */        kernel_w * kernel_h, 
-        /* Alpha: */    1,
-        /* *A: */       kernel2col, 
-        /* lda: */      kernel_h * kernel_w,
-        /* *B: */       srcIm2col,
-        /* ldb: */      outHeight * outWidth, 
-        /* beta: */     0, 
-        /* *C: */       output, 
-        /* ldc: */      outHeight * outWidth
-    );
+            /* M: */        out_channels,
+            /* N: */        outHeight * outWidth,
+            /* K: */        kernel_w * kernel_h,
+            /* Alpha: */    1,
+            /* *A: */       kernel2col,
+            /* lda: */      kernel_h * kernel_w,
+            /* *B: */       srcIm2col,
+            /* ldb: */      outHeight * outWidth,
+            /* beta: */     0,
+            /* *C: */       output,
+            /* ldc: */      outHeight * outWidth
+        );
+
+        // Cycle cleanup
+        delete[] srcIm2col;
+        delete[] output;
+    }
 
     // 结束计时
     auto end = std::clock();
-    int repeat_count = 1;
     float time_ms = 1000.0 * (end - start) / CLOCKS_PER_SEC / 1000.0 / repeat_count;
-    
-    cout<<"im2colOrigin Total time cost: " << time_ms <<" ms"<<endl;
 
-    // 
-    delete [] kernel2col;
-    delete [] srcIm2col;
-    delete [] output;
+    // Cleanup
+    delete[] kernel2col;
 
-    for(int i = 0; i < kernel_num; i++){
-        for(int j = 0; j < kernel_h; j++){
-            delete [] kernel[i][j];
+    for (int i = 0; i < out_channels; i++) {
+        for (int j = 0; j < kernel_h; j++) {
+            delete[] kernel[i][j];
         }
-        delete [] kernel[i];
+        delete[] kernel[i];
     }
+    delete[] kernel;
 
-    for(int i = 0; i < inHeight; i++){
-        delete [] src[i];
+    for (int i = 0; i < in_height; i++) {
+        delete[] src[i];
     }
+    delete[] src;
+
+    return time_ms;
+}
+
+
+int main()
+{
+    float time_ms = measure_im2col_time(100);
+
+    cout<<"im2colOrigin Total time cost: " << time_ms <<" ms"<<endl;
 
     return 0;
 }
